bdf680eaccd7f155c293a1ea95b36fa6cd583f51
Aiden <zjnuszt@126.com>
2022-08-05 16:17:20 +0800

    rttm

--- pyannote-audio/test.py
+++ pyannote-audio/test.py
@@ -2,8 +2,12 @@ from pyannote.audio import Pipeline
 pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization@2022.07")
 
 # apply the pipeline to an audio file
-diarization = pipeline("audio.wav")
+diarization = pipeline("/media/aiden/D83891573891358A/datasets/bravd.wav")
 
-# # dump the diarization output to disk using RTTM format
-# with open("audio.rttm", "w") as rttm:
-#     diarization.write_rttm(rttm)
+# dump the diarization output to disk using RTTM format
+with open("bravd.rttm", "w") as rttm:
+    diarization.write_rttm(rttm)
+
+with open("bravd.rttm", "r", encoding="utf-8") as f:
+    for line in f.readlines():
+        print(line)
